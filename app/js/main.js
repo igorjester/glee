b5ef26d6f7a880ca73272f174144138d2c3d3d79
Your Name <igorjester@mail.ru>
2022-01-27 18:23:58 +0200

    страница product-details

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,35 @@
 $(function () {
+
+$('.product-slide__thumb').slick({
+asNavFor: '.product-slide__big',
+ focusOnSelect: true,
+vertical: true,
+ slidesToShow: 3,
+ slidesToScroll: 1,
+ draggable: false,
+});
+
+$('.product-slide__big').slick({
+asNavFor: '.product-slide__thumb',
+ draggable: false,
+ arrows: false,
+ fade: true,
+});
+
   $('.top-slider__inner').slick({
   dots: true,
   arrows: false,
   });
+
+   $('.product-info__nam').styler();
+
+
+  $('.star').rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+
+   var mixer = mixitup('.product-goods__inner');
 });
